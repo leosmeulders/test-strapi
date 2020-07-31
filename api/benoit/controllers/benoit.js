225f2317619bc4c14873467a130364d708f898e2
Benoit <bduhamel@lamobilery.fr>
2020-07-31 17:20:11 +0200

    add model

--- api/benoit/controllers/benoit.js
+++ api/benoit/controllers/benoit.js
@@ -1,13 +1,8 @@
-'use strict'
+'use strict';
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {
-    async find(ctx){
-        console.log('bonjour')
-        return 'Salut Leo'
-    }
-}
+module.exports = {};
