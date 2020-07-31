5f7b8207d8d7e3a8f342b2bed728bf35f9da44ba
Léo Smeulders <lsmeulders@lamobilery.fr>
2020-07-31 16:19:15 +0200

    init

---
+++ api/client/models/client.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
+ * to customize this model
+ */
+
+module.exports = {
+  lifecycles: {
+    async afterFind(data) {
+      console.log(data)
+    }
+  }
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ae09a70c-8eb7-4eb6-8a84-5488d94fc835'
+};
